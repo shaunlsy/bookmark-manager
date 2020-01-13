7a70c2e5b15d78c126f3e1fcf5fd5141b20824f0
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-13 18:29:18 +0000

    setting up database

---
+++ Gemfile
@@ -0,0 +1,11 @@
+ruby '2.6.5'
+
+source 'https://rubygems.org'
+
+gem "rubocop", require: false
+gem "sinatra"
+
+gem "capybara", require: false, group: :test
+gem "rspec", require: false, group: :test
+gem "simplecov", require: false, group: :test
+gem "simplecov-console", require: false, group: :test
---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,16 @@
+feature 'Viewing bookmarks' do
+  feature 'visiting the homepage' do
+    scenario 'the page title is visible' do
+      visit '/'
+      expect(page).to have_content 'Bookmark Manager'
+    end
+  end
+
+  scenario 'shows a list of bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
+  
+end
