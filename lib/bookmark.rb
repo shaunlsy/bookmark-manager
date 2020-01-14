0b965c2e576bb73d8d23d69d32faf29acc3c6d35
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-14 14:07:06 +0000

    challenge 8 done

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,11 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-     ]
+    object = PG.connect(dbname: 'bookmark_manager')
+    result = object.exec('SELECT * FROM bookmarks')
+    result.map{ |bookmark| bookmark["url"] }
   end
 
 end
