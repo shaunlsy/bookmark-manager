a75e156d6c5068c279f851f3668e7c9839294755
Marcellofabbri <marcellogfabbri@gmail.com>
2020-01-14 15:14:56 +0000

    introduced environments

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,7 +3,12 @@ require 'pg'
 class Bookmark
 
   def self.all
-    object = PG.connect(dbname: 'bookmark_manager')
+    if ENV['ENVIRONMENT'] == 'test'
+      object = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      object = PG.connect(dbname: 'bookmark_manager')
+    end
+    
     result = object.exec('SELECT * FROM bookmarks')
     result.map{ |bookmark| bookmark["url"] }
   end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,12 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      object = PG.connect(dbname: 'bookmark_manager_test')
+
+      object.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      object.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      object.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
